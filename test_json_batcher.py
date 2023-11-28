33e9bcc32e949e38cef98a8557425fd027cece1c
Alex <aderhacobian@gmail.com>
2023-11-28 01:42:52 -0800

    update test script for batcher

--- test_json_batcher.py
+++ test_json_batcher.py
@@ -1,14 +1,13 @@
-from json_batcher import JSONBatcher
+from utils import JSONBatcher
 
 def generate_prompt(passage, schema):
-        user_message = f"""{passage}
-    
-From the above passage, extract the following schema:
-{schema}
+    user_message = f"""{passage}
+    From the above passage, extract the following schema: {schema}
 
-Only output JSON with the allowed types."""
-        prompt = f"""<s><<SYS>>You only respond in JSON. You do not add text before. You do not add text after. Only JSON.<</SYS>>[INST] {user_message} [/INST]"""
-        return prompt
+    Only output JSON with the allowed types."""
+    
+    prompt = f"""<s><<SYS>>You only respond in JSON. You do not add text before. You do not add text after. Only JSON. <</SYS>>[INST] {user_message} [/INST]"""
+    return prompt
 
 batcher = JSONBatcher('jsonbench.jsonl')
-print(batcher.get_batches(generate_prompt))
+data, schemas  = batcher.get_dataset(generate_prompt)
